b95a93f6baa57e97a0ef574fed653173833c60ce
G-dH <georgdh@gmail.com>
2023-12-25 13:17:05 +0100

    fix: Access to destroyed items from _onDestroy()

--- src/windowSwitcherPopup.js
+++ src/windowSwitcherPopup.js
@@ -719,8 +719,10 @@ class WindowSwitcherPopup extends SwitcherPopup.SwitcherPopup {
 
         // Make sure the SwitcherList is always destroyed, it may not be
         // a child of the actor at this point.
-        if (this._switcherList)
+        if (this._switcherList) {
+            this._switcherList._items = [];
             this._switcherList.destroy();
+        }
 
         // remove all local timeouts
         Object.values(this._timeoutIds).forEach(id => {
