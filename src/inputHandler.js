80de47cb73eb188cf2d03991fa1d5b5a6852e5a2
G-dH <georgdh@gmail.com>
2024-06-08 21:25:52 +0200

    inputHandler: Fix _clearSearchEntry()

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -502,9 +502,9 @@ var InputHandler = class {
     }
 
     _clearSearchEntry() {
-        if (!this._shiftPressed() && this._wsp._searchQuery !== null) {
+        if (!this._shiftPressed() && this._wsp._searchQuery) {
             this._wsp._searchQuery = '';
-            this._wsp.show();
+            this._wsp._updateSwitcher();
         }
     }
 
